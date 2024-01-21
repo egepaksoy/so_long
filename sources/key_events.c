ac45a0d28181adf7bbb497d97617149801a44125
Ege Paksoy <egepaskoy@gmail.com>
2024-01-21 09:59:25 +0300

    hazine acilmadan ustune gidilmeme eklendi

--- sources/key_events.c
+++ sources/key_events.c
@@ -6,7 +6,7 @@
 /*   By: epaksoy <epaksoy@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/11 15:00:24 by epaksoy           #+#    #+#             */
-/*   Updated: 2023/12/12 13:00:58 by epaksoy          ###   ########.fr       */
+/*   Updated: 2024/01/21 09:58:19 by epaksoy          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,13 +27,17 @@ int	key_codes(int key_code, t_mlx **mlx)
 
 	x = (*mlx)->player_x;
 	y = (*mlx)->player_y;
-	if (key_code == W && (*mlx)->map->map[y + 1][x] != '1' )
+	if (key_code == W && (*mlx)->map->map[y + 1][x] != '1' &&
+		key_code == W && (*mlx)->map->map[y + 1][x] != 'E')
 		go_up(mlx);
-	else if (key_code == D && (*mlx)->map->map[y][x + 1] != '1')
+	else if (key_code == D && (*mlx)->map->map[y][x + 1] != '1' &&
+		key_code == D && (*mlx)->map->map[y][x + 1] != 'E')
 		go_right(mlx);
-	else if (key_code == A && (*mlx)->map->map[y][x - 1] != '1')
+	else if (key_code == A && (*mlx)->map->map[y][x - 1] != '1' &&
+		key_code == A && (*mlx)->map->map[y][x - 1] != 'E')
 		go_left(mlx);
-	else if (key_code == S && (*mlx)->map->map[y - 1][x] != '1')
+	else if (key_code == S && (*mlx)->map->map[y - 1][x] != '1' &&
+		key_code == S && (*mlx)->map->map[y - 1][x] != 'E')
 		go_down(mlx);
 	else if (key_code == ESC)
 		esc_key(mlx);
